cfcbfd01cadef47775aa5962e23a0cdad9496a19
redrouthusiva <46279618+redrouthusiva@users.noreply.github.com>
2021-06-17 23:36:12 +0530

    Update feedback-page.component.ts

--- products-app/src/app/feedback-page/feedback-page.component.ts
+++ products-app/src/app/feedback-page/feedback-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback-page',
@@ -6,16 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback-page.component.css']
 })
 export class FeedbackPageComponent implements OnInit {
-  msg:string='';
+  feedbackForm: FormGroup;
+  msg: any;
+  submitted = false; 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { 
+    this.feedbackForm = this.formBuilder.group({
+      msg: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(100), Validators.pattern('^[a-zA-Z0-9_ ]*$')]],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
   }
-  res(){
-    this.msg='thank you for your feed back';
+  
+  ngOnInit() {
     
+  }
 
+  sendFeedback() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.feedbackForm.invalid) {
+      console.log('invalid');
+      return;
+    }
+    else{
+      this.msg = 'Your feedback is submitted successfully';
+      console.log(this.feedbackForm.value);
+    }
+    
   }
 
 }
